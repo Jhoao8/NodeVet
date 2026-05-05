cf6d2f51236060c7903626bef0007557ac3c1d19
Sebastian Ignacio Santander Leal <ssantander686@gmail.com>
2026-05-04 21:04:05 -0400

    estructura inicial frontend web

---
+++ Producto/front_web/src/interfaces/Usuario.tsx
@@ -0,0 +1,9 @@
+export interface Usuario{
+    id_usuario: number;
+    nombre_usr: string;
+    apellido_usr: string;
+    correo_usr:string;
+    password: string;
+    password_2: string;
+    
+}
