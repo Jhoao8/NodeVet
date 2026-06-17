6c0154fcdcf62dbb2eb02ca3454bfb0ab7303a92
Sebastian Ignacio Santander Leal <ssantander686@gmail.com>
2026-06-17 16:41:22 -0400

    Revert "creado modelo de bloque horario y de veterinario, agregada funcion de agregar veterinario y asignar bloque horario a veterinario para el rol de administrador."
    This reverts commit 3f703c26b25bf2ce4186fd901e2785f40cdb83e6.

--- Producto/backend/src/main/java/com/nodevet/app/repository/BloqueHorarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nodevet.app.repository;
-
-import com.nodevet.app.model.BloqueHorario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BloqueHorarioRepository extends JpaRepository<BloqueHorario, Integer> {
-}
